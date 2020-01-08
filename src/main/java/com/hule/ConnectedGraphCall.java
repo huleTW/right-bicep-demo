e46ad350ecc1c691060ba553407bb9539cc61042
Hu Le <le.hu.2010@163.com>
2020-01-08 15:08:32 +0800

    correct path

--- src/main/java/com/hule/ConnectedGraphCall.java
+++ src/main/java/com/hule/ConnectedGraphCall.java
@@ -3,30 +3,57 @@ package com.hule;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class ConnectedGraphCall {
+    private Map<String, List<String>> map;
 
-    public static void getGraphCallPath(List<String> currentPath, List<List<String>> totalPath,
-                                        String currentNode, Map<String, List<String>> relation,
-                                        String equalNode) {
-        if (!relation.containsKey(currentNode) || relation.get(currentNode).isEmpty()) {
+    public ConnectedGraphCall(Map<String, List<String>> map) {
+        this.map = map;
+    }
+
+    public List<String> getGraphCallPath() {
+        return map.keySet().stream().flatMap(k -> getConnectedPath(k).stream())
+                .collect(Collectors.toList());
+    }
+
+    private List<String> getConnectedPath(String startNode) {
+        List<String> relatedNodes = map.get(startNode);
+        List<String> result = new ArrayList<>();
+        ArrayList<String> currentPath = new ArrayList<>();
+        for (int i = 0; i < relatedNodes.size(); i++) {
+            for (int j = i + 1; j < relatedNodes.size(); j++) {
+                getPath(startNode, currentPath, result, relatedNodes.get(i), relatedNodes.get(j));
+                getPath(startNode, currentPath, result, relatedNodes.get(j), relatedNodes.get(i));
+            }
+        }
+        return result;
+    }
+
+    private void getPath(String startNode,
+                         List<String> currentPath,
+                         List<String> totalPath,
+                         String currentNode,
+                         String equalNode) {
+        currentPath.add(currentNode);
+        if (!map.containsKey(currentNode) || map.get(currentNode).isEmpty()
+                || currentNode.equals(startNode) || currentNode.equals(equalNode)) {
+            currentPath.remove(currentPath.size() - 1);
             return;
         }
-        List<String> nextNodes = relation.get(currentNode);
+        List<String> nextNodes = map.get(currentNode);
 
         if (nextNodes.contains(equalNode)) {
             ArrayList<String> path = new ArrayList<>(currentPath);
-            path.add(currentNode);
             path.add(equalNode);
-            totalPath.add(path);
+            path.add(0, startNode);
+            totalPath.add(String.format("%s;%s->%s", String.join("->", path), startNode, equalNode));
         }
-        currentPath.add(currentNode);
-
         for (String node : nextNodes) {
             if (currentPath.contains(node)) {
                 continue;
             }
-            getGraphCallPath(currentPath, totalPath, node, relation, equalNode);
+            getPath(startNode, currentPath, totalPath, node, equalNode);
         }
         currentPath.remove(currentPath.size() - 1);
     }
--- src/test/java/com/hule/ConnectedGraphCallTest.java
+++ src/test/java/com/hule/ConnectedGraphCallTest.java
@@ -2,9 +2,13 @@ package com.hule;
 
 import org.junit.Test;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
+
 
 public class ConnectedGraphCallTest {
 
@@ -12,13 +16,12 @@ public class ConnectedGraphCallTest {
     public void should_get_connected_graph_call_path() {
         Map<String, List<String>> map = new HashMap<>();
         map.put("A", Arrays.asList("C", "B", "D"));
-        map.put("B", Arrays.asList("D","H"));
+        map.put("B", Arrays.asList("D", "H"));
         map.put("C", Arrays.asList("D", "M"));
         map.put("D", Arrays.asList("E", "B"));
         map.put("E", Arrays.asList("B", "D"));
-        List<List<String>> total = new ArrayList<>();
-        ConnectedGraphCall.getGraphCallPath(new ArrayList<>(), total, "C", map, "B");
-        assertEquals(total.size(), 2);
+        List<String> total = new ConnectedGraphCall(map).getGraphCallPath();
+        assertEquals(total.size(), 20);
     }
 
 }
